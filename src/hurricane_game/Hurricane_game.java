6122d25e8b21cd7bfa4dc2b654be975080306ee8
Maxine <Maxine@Sini>
2015-12-04 00:50:08 -0800

    Added private static PrintWriter outFile, private static BufferedReader inFile, private static PrintWriter logFile and anything else associated with them.

--- src/hurricane_game/Hurricane_game.java
+++ src/hurricane_game/Hurricane_game.java
@@ -12,6 +12,12 @@ import model.StoreAislesCamping;
 import model.StoreCamping;
 import view.StoreMegaAislesView;
 import control.GameControl;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  * @author maloriegomm
  */
@@ -20,7 +26,31 @@ public class Hurricane_game {
     private static GameControl createNewGame = null;
     private static Player player = null;
     
+    private static PrintWriter outFile = null;
+    private static BufferedReader inFile = null;
+
+    private static PrintWriter logFile = null;
+    
     public static void main(String[] args) {
+        
+        try {
+            
+            // open character stream files for end user input and output
+            Hurricane_game.inFile = 
+                    new BufferedReader(new InputStreamReader(System.in));
+            
+            Hurricane_game.outFile = new PrintWriter(System.out, true);
+        
+        try {
+            // open log file
+            String filePath = "log.txt";
+            Hurricane_game.logFile = new PrintWriter(filePath);
+            
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.toString() +
+                    "\nCause: " + e.getCause() +
+                    "\nMessage: " + e.getMessage());
+        }
                
 //        Player player = new Player();
 //        
@@ -32,25 +62,64 @@ public class Hurricane_game {
 //        welcomeView.displayPlayerNameBanner(player);
 //        
          MainMenuView mainView = new MainMenuView();
-         try {
          mainView.display();
-         } catch (Throwable te) {
-             System.out.println(te.getMessage());
-             te.printStackTrace();
-             mainView.display();
-         }
+         return;
+         
+        } catch (Throwable e) {
+             System.out.println("Exception: " + e.toString() +
+                     "\nCause: " + e.getCause() +
+                     "\nMessage: " + e.getMessage());
+             e.printStackTrace();;
+        }
         
+        finally {
+            try {
+                if (Hurricane_game.inFile != null)
+                    Hurricane_game.inFile.close();
+                
+                if (Hurricane_game.outFile != null)
+                    Hurricane_game.outFile.close();
+                
+                if (Hurricane_game.logFile != null)
+                    Hurricane_game.logFile.close();
+            } catch (IOException ex) {
+                System.out.println("Error closing files");
+                return;
+            }
+            
+        }
+         
 //        char in = mainView.getInput();
 //        
 //        System.out.println("YOU ENTERED " + in);
 
         }
+    
+    public static PrintWriter getOutFile() {
+        return outFile;
+    }
+
+    public static void setOutFile(PrintWriter outFile) {
+        Hurricane_game.outFile = outFile;
+    }
+
+    public static BufferedReader getInFile() {
+        return inFile;
+    }
+
+    public static void setInFile(BufferedReader inFile) {
+        Hurricane_game.inFile = inFile;
+    }
+
+    public static PrintWriter getLogFile() {
+        return logFile;
+    }
+
+    public static void setLogFile(PrintWriter logFile) {
+        Hurricane_game.logFile = logFile;
     }
     
-    
-    
-    
-    
+}
     
 //    /**
 //     * @param args the command line arguments
