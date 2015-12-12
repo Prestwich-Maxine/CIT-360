f95ad05e9a739deeac0f0f378485d496d289e479
Maxine <Maxine@Sini>
2015-12-11 23:34:26 -0800

    Changed checkDeliveries method but something's still off.

--- src/control/DeliveryControl.java
+++ src/control/DeliveryControl.java
@@ -12,54 +12,46 @@ import Exception.DeliveryControlException;
  * @author maloriegomm
  */
 public class DeliveryControl {
-    
-    public int checkDeliveries(boolean makeDeliveries, int numberOfDeliveries) {
-    
-        int returnValue;
-        
-        if (!makeDeliveries) {
-            
-            returnValue = 0;
-        }
-        
-	else {
-            for (int d = 10; d <= 10; d--) // need to rewrite this code; not working
-                
-                numberOfDeliveries = 10 - d--;
-            
-            returnValue = numberOfDeliveries;
 
-            }
-        return returnValue;
+    public void checkDeliveries(int numberOfDeliveries) {
+
+        for (int d = 10; d >= 0; d--) {
+            int deliveries = d--;
+            
+           if (deliveries > 0) {
+               
+               System.out.println("You have this many deliveries available:"
+                       + deliveries);
+           } else
+               
+               System.out.println("Currently, there are no deliveries to make.");
         }
-    
-    
-    public int calcNewTimeAfterDelivery(boolean makeDeliveries, int timeRemaining) 
-            throws DeliveryControlException {
         
+    }
+
+    public int calcNewTimeAfterDelivery(boolean makeDeliveries, int timeRemaining)
+            throws DeliveryControlException {
+
         if (!makeDeliveries) {
-		return timeRemaining;
-        }
-        else {
-            if (makeDeliveries) {
-                
-                try {
-                int deliveryTime = timeRemaining - 2; // deduct 2 hours from game every time a delivery is 
+            return timeRemaining;
+        } else if (makeDeliveries) {
+
+            try {
+                int deliveryTime = timeRemaining - 1; // deduct 2 hours from game every time a delivery is 
                 return deliveryTime;
-                } catch (NumberFormatException nfe) {
-                    System.out.println("Your time must be greater than 0.");
-                }
-                
+            } catch (NumberFormatException nfe) {
+                System.out.println("You are out of time. Game over.");
             }
+
         }
         int deliveryTime = timeRemaining - 2;
-               
-	if (deliveryTime < 0) {
-		throw new DeliveryControlException("You are out of delivery time.");
+
+        if (deliveryTime < 0) {
+            throw new DeliveryControlException("You are out of delivery time.");
         }
-	
-	return deliveryTime;
-    }
 
+        return deliveryTime;
     }
+    
 
+}
