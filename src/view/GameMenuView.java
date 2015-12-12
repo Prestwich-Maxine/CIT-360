9c4d69ca6304738588b8a527debf7e3eec37959e
Maxine <Maxine@Sini>
2015-12-11 23:50:22 -0800

    Working on items but not finished yet.

--- src/view/GameMenuView.java
+++ src/view/GameMenuView.java
@@ -14,6 +14,7 @@ import model.Location;
 import model.LocationName;
 import model.MainMap;
 import model.StoreAislesCamping;
+import model.Item;
 
 /**
  *
@@ -211,6 +212,8 @@ public class GameMenuView extends View {
     }
 
     private void showAllItems() {
+        
+         
         
         }
 }
