f725d733bda362fcfbc034095632ae3b74e2b55b
Maxine <Maxine@Sini>
2015-12-11 23:39:51 -0800

    Checked this for grammar/punctuation errors. Looks good.

--- src/view/HelpMenuView.java
+++ src/view/HelpMenuView.java
@@ -57,33 +57,37 @@ public class HelpMenuView extends View{
      
     
      private void GameGoal() {
-        System.out.println("The goal of the game is to.../n"
-                             + ""); 
+        System.out.println("The goal of the game is to gather all your supplies\n"
+                + "before your time runs out. Remember, you start with 72 hours,\n"
+                + "some supplies, and some funds to purchase needed items. You will\n"
+                + "be able to earn more money as needed. However, as you move\n"
+                + "from one location to another or make deliveries, time will be\n"
+                + "deducted from your game.\n"); 
     }
     
     private void ViewInventory() {
-        System.out.println("To view your inventory, you will need to go to the Game Menu/n"
-                            + "and select 'I' for veiw inventory/n"
-                            + ""); 
+        System.out.println("To view your inventory list, you will need to go to\n"
+                + "the Game Menu and select 'I' for View Inventory. This list\n"
+                + "displays the items you start the game with. You will be able"
+                + "to keep track of items you need since they will be added to this"
+                + "list as purchases are made.\n"); 
     }
 
     private void MoveLocations() {
-        System.out.println("To move locations on the map, determine where you want to move/n"
-                            + " to and then go to the Game Menu and /n"
-                            + "select 'L' for Move to a New Location./n"
-                             + "");  
+        System.out.println("To move locations on the map, determine where you want\n "
+                + "to move. Then, go to the Game Menu and select 'L' for Move to\n"
+                + "a New Location.\n");  
     }
 
     private void MakeMoney() {
-        System.out.println("To make money you will need to go to the delivery center and accept a delivery/n"
-                            + "and then make that delivery in the delivery center./n"
-                             + ""); 
+        System.out.println("To make money, you will need to go to the delivery\n"
+                + "center, choose a delivery,and then make that delivery in the\n"
+                + "delivery center.\n"); 
     }
 
     private void BuySupplies() {
-        System.out.println("To purchase supplies you will need to go to the Mega store and/or the camping store./n"
-                            + "You will be able to select which aisle you want to go down/n"
-                            + "and then purchase those items by selecting them in the store aisles./n"
-                            + "");   
+        System.out.println("To purchase supplies you will need to go to the Mega\n"
+                + "Store and the Camping Store. You will be able to select\n"
+                + "an aisle and then purchase items in the store aisles.\n");   
     }
 }
