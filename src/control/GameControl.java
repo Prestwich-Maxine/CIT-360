9fa7f62da99f0a57af739b12588e79be739ddcec
Maxine <Maxine@Sini>
2015-12-14 16:50:24 -0800

    Called items in Item class but used a funny way to get them to display. They are displayed, but it's not best practice. Might change it.

--- src/control/GameControl.java
+++ src/control/GameControl.java
@@ -40,7 +40,7 @@ public class GameControl {
 
         MainMap map = new MainMap();
         game.setMap(map);
-        
+
         player.setLocation(map.getStartingLocation());
     }
 
@@ -78,114 +78,98 @@ public class GameControl {
         }
     }
 
-    public static Item[] createListOfAllItems() {
-        System.out.println("Food\n");
-        Item.BEANS.getItemDescription();
-        Item.BEANS.getItemCost();
-        Item.BEANS.getSurvivalPoints();
+    public static Item[] getAllItems() {
+
+        /*for (Item product : Item.values()) {
+            System.out.println(product);
+        }*/
+
+        System.out.println("Food:");
         
-        Item.BREAD.getItemDescription();
-        Item.BREAD.getItemCost();
-        Item.BREAD.getSurvivalPoints();
+        System.out.println(Item.BEANS.getItemDescription() + " $"
+                + Item.BEANS.getItemCost() + " " + Item.BEANS.getSurvivalPoints());
         
-        Item.CANDY.getItemDescription();
-        Item.CANDY.getItemCost();
-        Item.CANDY.getSurvivalPoints();
+        System.out.println(Item.BREAD.getItemDescription() + " $" +
+                Item.BREAD.getItemCost() + " " + Item.BREAD.getSurvivalPoints());
         
-        Item.FRUIT.getItemDescription();
-        Item.FRUIT.getItemCost();
-        Item.FRUIT.getSurvivalPoints();
+        System.out.println(Item.CANDY.getItemDescription() +" $" +
+                Item.CANDY.getItemCost() + " " + Item.CANDY.getSurvivalPoints());
         
-        Item.CANNED_MEAT.getItemDescription();
-        Item.CANNED_MEAT.getItemCost();
-        Item.CANNED_MEAT.getSurvivalPoints();
+        System.out.println(Item.FRUIT.getItemDescription() +" $" +
+                Item.FRUIT.getItemCost() + " " + Item.FRUIT.getSurvivalPoints());
         
-        Item.VEGETABLES.getItemDescription();
-        Item.VEGETABLES.getItemCost();
-        Item.VEGETABLES.getSurvivalPoints();
+        System.out.println(Item.TUNA.getItemDescription() +" $" +
+                Item.TUNA.getItemCost() + " " + Item.TUNA.getSurvivalPoints());
         
-        Item.CHOCOLATE.getItemDescription();
-        Item.CHOCOLATE.getItemCost();
-        Item.CHOCOLATE.getSurvivalPoints();
+        System.out.println(Item.VEGETABLES.getItemDescription() +" $" +
+                Item.VEGETABLES.getItemCost() + " " + Item.VEGETABLES.getSurvivalPoints());
         
-        Item.COOKIES.getItemDescription();
-        Item.COOKIES.getItemCost();
-        Item.COOKIES.getSurvivalPoints();
+        System.out.println(Item.CHOCOLATE.getItemDescription() +" $" +
+                Item.CHOCOLATE.getItemCost() + " " + Item.CHOCOLATE.getSurvivalPoints());
         
-        Item.CRACKERS.getItemDescription();
-        Item.CRACKERS.getItemCost();
-        Item.CRACKERS.getSurvivalPoints();
+        System.out.println(Item.COOKIES.getItemDescription() +" $" +
+                Item.COOKIES.getItemCost() + " " + Item.COOKIES.getSurvivalPoints());
         
-        Item.GRANOLA_BARS.getItemDescription();
-        Item.GRANOLA_BARS.getItemCost();
-        Item.GRANOLA_BARS.getSurvivalPoints();
+        System.out.println(Item.CRACKERS.getItemDescription() +" $" +
+                Item.CRACKERS.getItemCost() + " " + Item.CRACKERS.getSurvivalPoints());
         
-        Item.PROTEIN_BAR.getItemDescription();
-        Item.PROTEIN_BAR.getItemCost();
-        Item.PROTEIN_BAR.getSurvivalPoints();
+        System.out.println(Item.GRANOLA_BARS.getItemDescription() +" $" +
+                Item.GRANOLA_BARS.getItemCost() + " " + Item.GRANOLA_BARS.getSurvivalPoints());
         
-        Item.WATER.getItemDescription();
-        Item.WATER.getItemCost();
-        Item.WATER.getSurvivalPoints();
+        System.out.println(Item.PROTEIN_BAR.getItemDescription() +" $" +
+                Item.PROTEIN_BAR.getItemCost() + " " + Item.PROTEIN_BAR.getSurvivalPoints());
         
-        System.out.println("Clothes:\n");
-        Item.JACKET.getItemDescription();
-        Item.JACKET.getItemCost();
-        Item.JACKET.getSurvivalPoints();
+        System.out.println(Item.WATER.getItemDescription() +" $" +
+                Item.WATER.getItemCost() + " " + Item.WATER.getSurvivalPoints() + "\n");
         
-        Item.SHOES.getItemDescription();
-        Item.SHOES.getItemCost();
-        Item.SHOES.getSurvivalPoints();
+        System.out.println("Clothes:");
         
-        Item.SWEATSHIRT.getItemDescription();
-        Item.SWEATSHIRT.getItemCost();
-        Item.SWEATSHIRT.getSurvivalPoints();
+        System.out.println(Item.JACKET.getItemDescription() +" $" +
+                Item.JACKET.getItemCost() + " " + Item.JACKET.getSurvivalPoints());
         
-        System.out.println("Surival/Camping:\n");
-        Item.TARP.getItemDescription();
-        Item.TARP.getItemCost();
-        Item.TARP.getSurvivalPoints();
+        System.out.println(Item.SHOES.getItemDescription() +" $" +
+                Item.SHOES.getItemCost() + " " + Item.SHOES.getSurvivalPoints());
         
-        Item.TENT.getItemDescription();
-        Item.TENT.getItemCost();
-        Item.TENT.getSurvivalPoints();
+        System.out.println(Item.CANDY.getItemDescription() +" $" +
+                Item.CANDY.getItemCost() + " " + Item.CANDY.getSurvivalPoints());
+
+        System.out.println(Item.SWEATSHIRT.getItemDescription() +" $" +
+                Item.SWEATSHIRT.getItemCost() + " " + Item.SWEATSHIRT.getSurvivalPoints() + "\n");
         
-        Item.CAMP_STOVE.getItemDescription();
-        Item.CAMP_STOVE.getItemCost();
-        Item.CAMP_STOVE.getSurvivalPoints();
+        System.out.println("Camping/Survival:");
         
-        Item.FUEL.getItemDescription();
-        Item.FUEL.getItemCost();
-        Item.FUEL.getSurvivalPoints();
+        System.out.println(Item.TARP.getItemDescription() +" $" +
+                Item.TARP.getItemCost() + " " + Item.TARP.getSurvivalPoints());
+
+        System.out.println(Item.TENT.getItemDescription() +" $" +
+                Item.TENT.getItemCost() + " " + Item.TENT.getSurvivalPoints());
+
+        System.out.println(Item.CAMP_STOVE.getItemDescription() +" $" +
+                Item.CAMP_STOVE.getItemCost() + " " + Item.CAMP_STOVE.getSurvivalPoints());
         
-        System.out.println("Personal Care:\n");
-        Item.HYGIENE_KIT.getItemDescription();
-        Item.HYGIENE_KIT.getItemCost();
-        Item.HYGIENE_KIT.getSurvivalPoints();
+        System.out.println(Item.FUEL.getItemDescription() +" $" +
+                Item.FUEL.getItemCost() + " " + Item.FUEL.getSurvivalPoints());
         
-        Item.MEDICINE.getItemDescription();
-        Item.MEDICINE.getItemCost();
-        Item.MEDICINE.getSurvivalPoints();
+        System.out.println(Item.HYGIENE_KIT.getItemDescription() +" $" +
+                Item.HYGIENE_KIT.getItemCost() + " " + Item.HYGIENE_KIT.getSurvivalPoints() +"\n");
         
-        Item.TOILET_PAPER.getItemDescription();
-        Item.TOILET_PAPER.getItemCost();
-        Item.TOILET_PAPER.getSurvivalPoints();
-                
+        System.out.println("Personal Care:");
         
-        
-        
-        
-        
-               
-           
-        
+        System.out.println(Item.MEDICINE.getItemDescription() +" $" +
+                Item.MEDICINE.getItemCost() + " " + Item.MEDICINE.getSurvivalPoints());
+
+        System.out.println(Item.TOILET_PAPER.getItemDescription() +" $" +
+                Item.TOILET_PAPER.getItemCost() + " " + Item.TOILET_PAPER.getSurvivalPoints());
+
         return null;
     }
+
     public static InventoryList[] createItemsCurrent() {
-        
+
         return null;
 
     }
+
     public static InventoryList[] createItemsNeeded() {
         System.out.println("***Items the player still needs***");
         //this.console.println(this.message);
@@ -195,6 +179,5 @@ public class GameControl {
     public void initializeMap() {
 
     }
-    
+
 }
-    
