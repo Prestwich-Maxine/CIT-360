1b55be84d0cdb1adecf2b201c66f966fdfeb4ba4
Maxine <Maxine@Sini>
2015-12-16 18:51:45 -0800

    I changed private int money to private double money to keep everything consistent.

--- src/model/Player.java
+++ src/model/Player.java
@@ -18,7 +18,7 @@ public class Player implements Serializable{
     
     private String name;
     private List<Item> inventory;
-    private int money;
+    private double money;
     private List<Deliveries> deliveries;
     public String itemsCurrent [];
     public int timeRemaining;
@@ -64,11 +64,11 @@ public class Player implements Serializable{
         this.inventory = inventory;
     }
 
-    public int getMoney() {
+    public double getMoney() {
         return money;
     }
 
-    public void setMoney(int money) {
+    public void setMoney(double money) {
         this.money = money;
     }
     
