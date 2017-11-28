c634a05171ca1c2a5e897457b3c4a531a28318b5
Prestwich-Maxine <toi12001@byui.edu>
2017-11-28 14:02:00 -0800

    App1, App2, Ap4 and Worker App5
    App1 and App2 - Shows the two different basic ways to create threads
    App4 and Worker - Show how to use synchronization for more than one thread
    App5 - Use of Runnable, Executor and threading

---
+++ Threading/Worker.java
@@ -0,0 +1,95 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Maxine
+ */
+/*  Having more than one intrinisic log so two different threads can 
+*   write at the same time since they are not writing to the same list.
+*   Create two different objects, lock1 and lock2 in this case.
+ */
+public class Worker {
+
+    private Random random = new Random();
+
+    private Object lock1 = new Object();
+    private Object lock2 = new Object();
+
+    private List<Integer> list1 = new ArrayList<>();
+    private List<Integer> list2 = new ArrayList<>();
+
+    public void stageOne() {
+
+        synchronized (lock1) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            list1.add(random.nextInt(100));
+        }
+    }
+
+    public void stageTwo() {
+
+        synchronized (lock2) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            list2.add(random.nextInt(100));
+        }
+    }
+
+    public void process() {
+        for (int i = 0; i < 1000; i++) {
+            stageOne();
+            stageTwo();
+        }
+    }
+
+    public void main() {
+        System.out.println("Starting");
+        long start = System.currentTimeMillis();
+
+        Thread t1 = new Thread(new Runnable() {
+
+            public void run() {
+                process();
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+
+            public void run() {
+                process();
+            }
+        });
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time taken: " + (end - start));
+        System.out.println("List1: " + list1.size() + "; List2: " + list2.size());
+    }
+
+}
